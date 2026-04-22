814c54dc3aa0f9b56e148225097481e5debdf3f5
Jurian Andriessen <jurianandriessen@Mac-mini-van-IDK-035-883.local>
2026-04-22 09:29:40 +0200

    chore(feat): jobs can be fetched from the queue and can be processed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import requests
 from dotenv import load_dotenv
 
@@ -7,39 +8,52 @@ load_dotenv()
 API_URL = os.getenv('LARAVEL_API_URL')
 TOKEN = os.getenv('WORKER_TOKEN')
 
-JOB_ID = "01kpr67v3j083yvawrd599xqe8" 
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
 
-def report_to_laravel():
-    print(f"Worker is starting job: {JOB_ID}")
-
-    endpoint = f"{API_URL}/scraper-jobs/{JOB_ID}/status"
+def poll_for_jobs():
+    print("Looking for jobs with status QUEUED \n")
     
-    headers = {
-        "Authorization": f"Bearer {TOKEN}",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    
-    payload = {
-        "status": "COMPLETED" 
-    }
-
-    try:
-        response = requests.patch(endpoint, json=payload, headers=headers)
-
-        if response.status_code == 200:
-            print(response.json()['message'])
-        elif response.status_code == 403:
-            print("403")
-        elif response.status_code == 422:
-            print("422")
-            print(response.json())
-        else:
-            print(f"Error: status Code: {response.status_code}")
-            print(response.text)
-
-    except requests.exceptions.ConnectionError:
-        print("Laravel not running probably")
+    while True:
+        try:
+            response = requests.get(f"{API_URL}/scraper-jobs/next", headers=HEADERS)
+            
+            if response.status_code == 200:
+                data = response.json()
+                job = data['job']
+                job_id = job['id']
+                
+                print(f"{job_id} status -> PROCESSING")
+                
+               # todo: add the scraping logic
+                time.sleep(3) 
+                
+                final_status = "COMPLETED" 
+                
+                payload = {"status": final_status}
+                update_res = requests.patch(f"{API_URL}/scraper-jobs/{job_id}/status", json=payload, headers=HEADERS)
+                
+                if update_res.status_code == 200:
+                    print(f"{job_id} status -> {final_status}.\n")
+                else:
+                    print(f"{update_res.text}")
+                
+            elif response.status_code == 404:
+                pass
+                
+            elif response.status_code == 403:
+                print("No rights to update the status of a job 403")
+                
+            else:
+                print(f"{response.status_code} - {response.text}")
+                
+        except requests.exceptions.ConnectionError:
+            print("Cannot reach Laravel")
+            
+        time.sleep(5) 
 
 if __name__ == "__main__":
-    report_to_laravel()
+    poll_for_jobs()
